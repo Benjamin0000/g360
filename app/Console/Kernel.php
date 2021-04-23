632e75a3363f35489014dcdd5e7fd41d3df18478
Home <you@example.com>
2021-04-23 12:52:00 +0300

    made a change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function(){
             Task::gsClub();
-        })->everyFiveMinutes();
+        })->everyMinute();
         
 
         $schedule->call(function(){
